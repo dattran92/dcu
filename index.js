62d350946a42d5518248ed5485a1be498b0d0fdb
Dat Tran <tranthanhdat.it@gmail.com>
2023-12-12 12:19:42 +0700

    Fix script

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const pkg = require('./package.json');
 const render = require('./utils/render');
+const { snakecase } = require('stringcase');
 
 const program = new Command();
 
@@ -15,21 +16,35 @@ program
 
 program.command('create-component')
   .argument('<name>', 'Component Name')
+  .option('-p, --path [path]', 'Path to component')
   .option('--no-css', 'Ignore css file')
-  .action((name, options) => {    
-    fs.mkdirSync(name);
+  .option('--no-locale', 'Ignore locale file')
+  .action((name, options) => {
+    const basePath = path.join(options.path || '', name);
+    fs.mkdirSync(basePath);
 
     // index.js
     const indexContent = `import { default } from './${name}'`;
-    fs.writeFileSync(path.join(name, 'index.js'), indexContent, 'utf8');
+    fs.writeFileSync(path.join(basePath, 'index.js'), indexContent, 'utf8');
 
     // <name>.js
     const componentContent = render('CreateComponent', { name });
-    fs.writeFileSync(path.join(name, `${name}.js`), componentContent, 'utf8');
+    fs.writeFileSync(path.join(basePath, `${name}.js`), componentContent, 'utf8');
 
+    // <name>.module.scss
     if (options.css) {
-      // <name>.module.scss
-      fs.writeFileSync(path.join(name, `${name}.module.scss`), '', 'utf8');
+      fs.writeFileSync(path.join(basePath, `${name}.module.scss`), '', 'utf8');
+    }
+
+    // locales/en.json
+    if (options.locale) {
+      fs.mkdirSync(path.join(basePath, 'locales'));
+      const localeContent = render('locale', { name: snakecase(name) });
+      fs.writeFileSync(
+        path.join(basePath, 'locales', 'en.json'), 
+        localeContent, 
+        'utf8',
+      );
     }
   });
 
